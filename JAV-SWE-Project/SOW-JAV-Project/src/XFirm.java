c4ec45ed3b91b899e589d506593b5bbcc56aa044
srand <randip.singh@edu.bib.de>
2024-07-02 15:21:34 +0200

    Methoden zur XFirm vollständig

--- JAV-SWE-Project/SOW-JAV-Project/src/XFirm.java
+++ JAV-SWE-Project/SOW-JAV-Project/src/XFirm.java
@@ -1,3 +1,5 @@
+import java.time.LocalDate;
+
 public class XFirm {
     private Mitarbeiter[] angestelltenListe = new Mitarbeiter[20];
     private Mitarbeiter[] ehemaligenListe;
@@ -71,6 +73,86 @@ public class XFirm {
             }
         }
     }
+    public double MinLohn(){
+        double temp = MaxLohn();
+        for (int i = 0; i < angestelltenListe.length; i++) {
+            if (angestelltenListe[i] != null) {
+                if (angestelltenListe[i].GehaltBerechnen() < temp){
+                    temp = angestelltenListe[i].GehaltBerechnen();
+                }
+            } else if (angestelltenListe[i] == null) {
+                System.out.println("The List is empty at index " + i);
+                break;
+            }
+        }
+        return temp;
+    }
+    public double MaxLohn(){
+        double temp = 0.0;
+        for (int i = 0; i < angestelltenListe.length; i++) {
+            if (angestelltenListe[i] != null) {
+                if (angestelltenListe[i].GehaltBerechnen() > temp){
+                    temp = angestelltenListe[i].GehaltBerechnen();
+                }
+            } else if (angestelltenListe[i] == null) {
+                System.out.println("The List is empty at index " + i);
+                break;
+            }
+        }
+        return temp;
+    }
 
-    
+    public double VerdienstSpanne(){
+        return MaxLohn() - MinLohn();
+    }
+    public double DurchschittsAlter(){
+        double temp = 0.0;
+        for (int i = 0; i < angestelltenListe.length; i++) {
+            if (angestelltenListe[i] != null) {
+                temp += angestelltenListe[i].getGeburtsdatum().getYear();
+            } else if (angestelltenListe[i] == null) {
+                System.out.println("The List is empty at index " + i);
+                break;
+            }
+        }
+        return temp / getAnzahlMitarbeiter();
+    }
+    public int LaengsterMitarbeiter(){
+        int temp = LocalDate.now().getYear();
+        for (int i = 0; i < angestelltenListe.length; i++) {
+            if (angestelltenListe[i] != null) {
+                if (angestelltenListe[i].getEintrittsdatum().getYear() < temp) {
+                    temp = angestelltenListe[i].getEintrittsdatum().getYear();
+                    System.out.println("The longest employee of the Company is: " + angestelltenListe[i].getVorname()+ " "+ angestelltenListe[i].getNachname());
+                }
+            }
+        }
+        return LocalDate.now().getYear() - temp;
+    }
+    public double GeschlechterAnteilW(){
+        int w = 0;
+        for (int i = 0; i < angestelltenListe.length; i++) {
+            if (angestelltenListe[i] != null) {
+                if (angestelltenListe[i].getGeschlecht() == 'w') {
+                    w++;
+                }
+            }
+            }
+        return (w / getAnzahlMitarbeiter()) * 100;
+    }
+public double GeschlechterAnteilM(){
+    int m = 0;
+    for (int i = 0; i < angestelltenListe.length; i++) {
+        if (angestelltenListe[i] != null) {
+            if (angestelltenListe[i].getGeschlecht() == 'm') {
+                m++;
+            } else if (angestelltenListe[i] == null) {
+                System.out.println("The List is empty at index " + i);
+                break;
+            }
+        }
+    }
+    return ((double) m / getAnzahlMitarbeiter()) * 100;
+}
+
 }
